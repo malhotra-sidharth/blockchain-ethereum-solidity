c6c9503c2ca9e98e59cdbc6a32f85ad14f860bd3
sidharth <sidharthmalhotra0094@gmail.com>
2018-05-02 03:50:32 -0400

    [WIP] Created a local copy of lottery contract inside react application

---
+++ lottery-react/src/lottery.js
@@ -0,0 +1,11 @@
+import web3 from './web3';
+
+// contract address
+const address = '0x4C9fF6498A1BA0cf0Ca33B812fBC7bFbD6F62bF2';
+
+// contract interface
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+// create a local copy of contract
+// for working from React side
+export default new web3.eth.Contract(abi, address);
