dc5332bffce46700386e65e686e40d9b8de71dbd
sidharth <sidharthmalhotra0094@gmail.com>
2018-05-02 04:23:04 -0400

    [WIP] Added people entered and amount to be won and text box for entering amount

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -5,16 +5,24 @@ import web3 from './web3';
 import lottery from './lottery';
 
 class App extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {manager: ''};
-  }
+
+  state = {
+      manager: '',
+      players: [],
+      balance: '',
+      value: '',
+  };
+
   async componentDidMount() {
     // No need to specify 'from' in call as inside react
     // by default it is the selected account in meta mask
     const manager = await lottery.methods.manager().call();
 
-    this.setState({manager: manager});
+    const players = await lottery.methods.getPlayers().call();
+
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({manager: manager, players: players, balance: balance});
   }
 
   render() {
@@ -22,6 +30,24 @@ class App extends Component {
       <div>
           <h2>Lottery Contract</h2>
           <p>This contract is managed by {this.state.manager}</p>
+          <p>There are {this.state.players.length} people entered,
+              competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!</p>
+
+          <hr/>
+
+          <form>
+              <h4>Want to try your luck?</h4>
+              <div>
+                  <label>
+                      Amout of ether to enter
+                  </label>
+                  <input
+                    value={this.state.value}
+                    onChange={event => this.setState({value: event.target.value})}
+                  />
+              </div>
+              <button>Enter</button>
+          </form>
       </div>
     );
   }
