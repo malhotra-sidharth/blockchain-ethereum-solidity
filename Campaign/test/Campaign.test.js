daa2063ec9025ae5b6e712c6e4c95d19bc403f8e
sidharth <sidharthmalhotra0094@gmail.com>
2018-05-06 18:02:06 -0400

    [WIP] Added tests

--- Campaign/test/Campaign.test.js
+++ Campaign/test/Campaign.test.js
@@ -28,10 +28,68 @@ beforeEach(async () => {
     // Assigns the first element from the array of addresses to
     // campaignAddress
     // just like campaignAddress = addresses[0]
-    [campaignAddress] = await factory.methods.getDeployedContracts().call();
+    [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
 
     campaign = await new web3.eth.Contract(
         JSON.parse(compiledCampaign.interface),
         campaignAddress
     );
+});
+
+
+describe('Campaigns', () => {
+    it('deploys a factory and a campaign', () => {
+        assert.ok(factory.options.address);
+        assert.ok(campaign.options.address);
+    });
+
+    it('marks caller as the campaign manager', async () => {
+        const manager = await campaign.methods.manager().call();
+        assert.equal(accounts[0], manager);
+    });
+
+    it('allows people to contribute money and marks them as approvers', async () => {
+        await campaign.methods.contribute().send({
+            value: '200',
+            from: accounts[1]
+        });
+
+        // check if accounts[1] exists in approvers or not
+        const isContributor = await campaign.methods.approvers(accounts[1]).call();
+
+        assert(isContributor);
+    });
+
+    it('requires a minimum contribution', async () => {
+        try {
+            await campaign.methods.contribute().send({
+                value: '5',
+                from: accounts[1]
+            });
+
+            // the above code should throw error
+            // and this line of code will always fail the test
+            // if this line of code is executed
+            assert(false);
+        }
+        catch (e) {
+            assert(e);
+        }
+    });
+
+    it('allows a manager to make a payment request', async () => {
+       // create a request
+       await campaign.methods
+           .createRequest('Buy Batteries', '100', accounts[1])
+           .send({
+               from: accounts[0], // manager address
+               gas: '1000000'
+           });
+
+       // get the request that was created by above code block
+        const request = await campaign.methods.requests(0).call();
+
+        console.log(request.description);
+        assert.equal('Buy Batteries', request.description);
+    });
 });
