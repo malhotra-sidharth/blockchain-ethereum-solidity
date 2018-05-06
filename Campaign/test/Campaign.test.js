6cf4926a80f8c50339696b60d03f30b741fabeb1
sidharth <sidharthmalhotra0094@gmail.com>
2018-05-06 17:17:23 -0400

    [WIP] Completed beforeEach

--- Campaign/test/Campaign.test.js
+++ Campaign/test/Campaign.test.js
@@ -18,4 +18,20 @@ beforeEach(async () => {
     factory = await  new web3.eth.Contract(JSON.parse(compiledFactory.interface))
         .deploy({data: compiledFactory.bytecode})
         .send({from: accounts[0], gas: '1000000'});
+
+    await factory.methods.createCampaign('100').send({
+        from: accounts[0],
+        gas: '1000000'
+    });
+
+    // get deployed contracts
+    // Assigns the first element from the array of addresses to
+    // campaignAddress
+    // just like campaignAddress = addresses[0]
+    [campaignAddress] = await factory.methods.getDeployedContracts().call();
+
+    campaign = await new web3.eth.Contract(
+        JSON.parse(compiledCampaign.interface),
+        campaignAddress
+    );
 });
