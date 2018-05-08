1466b53f75bc524915cece7cf295c4b2d00f7635
sidharth <sidharthmalhotra0094@gmail.com>
2018-05-08 17:31:14 -0400

    [FIX] CSS link fix

--- Campaign/pages/campaigns/new.js
+++ Campaign/pages/campaigns/new.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
+import Layout from '../../components/layout';
 
 class CampaignNew extends Component{
-    
+    render() {
+        return (
+            <Layout>
+                <h1>New Campaign!!</h1>
+            </Layout>
+        );
+    }
 }
 
 export default CampaignNew;
