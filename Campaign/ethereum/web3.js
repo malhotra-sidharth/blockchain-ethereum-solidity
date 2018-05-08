68614109694410aa725ee17808ea070e8e2f1fb1
sidharth <sidharthmalhotra0094@gmail.com>
2018-05-07 23:31:34 -0400

    [FIX] Fixed the erro 'window not defined'

--- Campaign/ethereum/web3.js
+++ Campaign/ethereum/web3.js
@@ -1,6 +1,28 @@
 import Web3 from 'web3';
 
 // get web3 provider from Metamask extension
-const web3 = new Web3(window.web3.currentProvider);
+// This will generate error 'window not defined'
+// because this gets executed on next server where
+// window is not defined as window is defined inside the
+// browser
+// const web3 = new Web3(window.web3.currentProvider);
+
+let web3;
+
+if (typeof window !== 'undefined' &&
+    typeof window.web3 !== 'undefined') {
+    // inside the browser and metamask is available
+    web3 = new Web3(window.web3.currentProvider);
+}
+else {
+    // we are on the server or the user is not running metamask
+    const provider = new Web3.providers.HttpProvider(
+        // NETWORK URL FROM INFURA
+        
+    );
+
+    web3 = new Web3(provider);
+}
+
 
 export default web3;
