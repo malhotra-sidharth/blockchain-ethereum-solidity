17d3f432a250d4d2789836ad2dcaa4ca65484a67
sidharth <sidharthmalhotra0094@gmail.com>
2018-05-08 21:37:36 -0400

    [WIP] Added textbox to new campaign page

--- Campaign/pages/campaigns/new.js
+++ Campaign/pages/campaigns/new.js
@@ -1,11 +1,31 @@
 import React, {Component} from 'react';
 import Layout from '../../components/layout';
+import { Form, Button, Input } from 'semantic-ui-react';
 
 class CampaignNew extends Component{
+    state = {
+        minimumContribution: ''
+    };
+
     render() {
         return (
             <Layout>
-                <h1>New Campaign!!</h1>
+                <h3>Create a Campaign</h3>
+
+                <Form>
+                    <Form.Field>
+                        <label>Minimum Contribution</label>
+                        <Input
+                            label="Wei"
+                            labelPosition="right"
+                            value={this.state.minimumContribution}
+                            onChange={event =>
+                                this.setState({minimunComtribution: event.target.value})}
+                        />
+                    </Form.Field>
+
+                    <Button primary>Create!</Button>
+                </Form>
             </Layout>
         );
     }
