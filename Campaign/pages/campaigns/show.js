782923e2f7d34a12da98868d68f89121cf3d4184
sidharth <sidharthmalhotra0094@gmail.com>
2018-05-08 23:41:04 -0400

    [WIP] Initialized show campaign details page

---
+++ Campaign/pages/campaigns/show.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Layout from '../../components/layout';
+
+class CampaignShow extends Component{
+    render() {
+        return (
+            <Layout>
+                <h1>CampaignShow!</h1>
+            </Layout>
+        );
+    }
+}
+
+export default CampaignShow;
--- Campaign/pages/index.js
+++ Campaign/pages/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import factory from '../ethereum/factory';
 import {Card, Button} from 'semantic-ui-react';
 import Layout from '../components/layout';
+import {Link} from '../routes';
 
 class CampaignIndex extends Component {
     static async getInitialProps() {
@@ -14,7 +15,11 @@ class CampaignIndex extends Component {
         const items = this.props.campaigns.map(address => {
             return {
                 header: address,
-                description: <a>View Campaign</a>,
+                description: (
+                    <Link route={`/campaigns/${address}`}>
+                        <a>View Campaign</a>
+                    </Link>
+                ),
                 fluid: true
             }
         });
@@ -28,12 +33,16 @@ class CampaignIndex extends Component {
             <Layout>
                 <div>
                     <h3>Open Campaigns</h3>
-                    <Button
-                        content="Create Campaign"
-                        icon="add circle"
-                        primary
-                        floated="right"
-                    />
+                    <Link route="/campaigns/new">
+                        <a>
+                            <Button
+                                content="Create Campaign"
+                                icon="add circle"
+                                primary
+                                floated="right"
+                            />
+                        </a>
+                    </Link>
                     {this.renderCampaigns()}
                 </div>
             </Layout>
